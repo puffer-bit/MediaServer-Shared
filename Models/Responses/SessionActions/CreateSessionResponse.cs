05c3883425db75c3e4b1c13e473bddfd96aa5ecb
puffer-bit <enki-xx2014@outlook.com>
2026-01-19 00:07:22 +0300

    Fixed bug

--- Models/Responses/SessionActions/CreateSessionResponse.cs
+++ Models/Responses/SessionActions/CreateSessionResponse.cs
@@ -1,12 +1,13 @@
 using Shared.Enums;
 using Shared.Models.DTO;
+using Shared.Models.Responses.Sessions;
 
 namespace Shared.Models.Responses.SessionActions;
 
 public record CreateSessionResponse(
     string RequestId,
     SessionDTO? Session,
-    CreateSessionResult Result ) : IResponse
+    CreateSessionResult Result ) : ISessionResponse
 {
-    public static SessionRequestType Type => SessionRequestType.Create;
+    public SessionRequestType Type => SessionRequestType.Create;
 }

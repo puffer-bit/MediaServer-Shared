dc8457e8b6f1a10be1aebbb8a3e3659b5c30b63c
puffer-bit <enki-xx2014@outlook.com>
2026-02-02 00:08:26 +0300

    Now peers in VideoSessionDTO.cs stores in IDictionary instead IList

--- Models/DTO/VideoSessionDTO.cs
+++ Models/DTO/VideoSessionDTO.cs
@@ -5,5 +5,5 @@ public class VideoSessionDTO : SessionDTO
     public bool IsHostConnected { get; set; }
     public bool IsAudioRequested { get; set; }
     public bool IsHostMustApprove { get; set; }
-    public IList<PeerDTO> Peers { get; set; }
+    public IDictionary<string, PeerDTO> Peers { get; set; }
 }

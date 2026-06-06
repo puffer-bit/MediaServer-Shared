77cafdcf90dd330aa120073b9fbb4a6bd6b6e820
puffer-bit <enki-xx2014@outlook.com>
2026-06-07 01:11:07 +0300

    Added JsonParser flags

--- Models/DTO/SessionDTO.cs
+++ Models/DTO/SessionDTO.cs
@@ -1,8 +1,14 @@
 ﻿
+using System.Text.Json.Serialization;
 using Shared.Enums;
+using Shared.Models.DTO.TextChat;
 
 namespace Shared.Models.DTO
 {
+    [JsonPolymorphic(TypeDiscriminatorPropertyName = "SessionType")]
+    [JsonDerivedType(typeof(TextChatDTO), typeDiscriminator: (int)SessionType.Chat)]
+    [JsonDerivedType(typeof(VideoSessionDTO), typeDiscriminator: (int)SessionType.Video)]
+    [JsonDerivedType(typeof(VoiceSessionDTO), typeDiscriminator: (int)SessionType.Voice)]
     public class SessionDTO
     {
         public required int Id { get; set; }
